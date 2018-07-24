ed7397a185d2bd4897aa04c424c279503a2a3818
tkominiak <41465450+tkominiak@users.noreply.github.com>
2018-07-24 16:39:43 +0100

    New Project

---
+++ src/main/java/sainsburys/Application.java
@@ -0,0 +1,10 @@
+package sainsburys;
+
+import org.springframework.boot.SpringApplication;
+
+public class Application {
+	
+	public static void main(String args[]) {
+		SpringApplication.run(SpringConfig.class);
+	}
+}
